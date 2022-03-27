ddb4f2cf068aec3fb21e908230b84891adefb8c7
modelsplaid <modelsplaid@qq.com>
2022-03-27 17:58:44 +0800

    added code to write speed,pos by usr. todo: use multi thread to get servo status and set servo

--- write_usr_input.py
+++ write_usr_input.py
@@ -1,1 +1,161 @@
 # todo: write to speified servo by usr input 
+#!/usr/bin/env python
+#
+# *********     Gen Write Example      *********
+#
+#
+# Available SCServo model on this example : All models using Protocol SCS
+# This example is tested with a SCServo(STS/SMS/SCS), and an URT
+# Be sure that SCServo(STS/SMS/SCS) properties are already set as %% ID : 1 / Baudnum : 6 (Baudrate : 1000000)
+#
+
+import os
+
+if os.name == 'nt':
+    import msvcrt
+    def getch():
+        return msvcrt.getch().decode()
+        
+else:
+    import sys, tty, termios
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    def getch():
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+from scservo_sdk import *                    # Uses SCServo SDK library
+
+# Control table address
+ADDR_SCS_TORQUE_ENABLE     = 40
+ADDR_SCS_GOAL_ACC          = 41
+ADDR_SCS_GOAL_POSITION     = 42
+ADDR_SCS_GOAL_SPEED        = 46
+ADDR_SCS_PRESENT_POSITION  = 56
+
+# Default setting
+SCS_ID                      = 1                 # SCServo ID : 1
+BAUDRATE                    = 1000000           # SCServo default baudrate : 1000000
+DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+SCS_MINIMUM_POSITION_VALUE  = 2000         # SCServo will rotate between this value
+SCS_MAXIMUM_POSITION_VALUE  = 3000        # and this value (note that the SCServo would not move when the position value is out of movable range. Check e-manual about the range of the SCServo you use.)
+SCS_MOVING_STATUS_THRESHOLD = 20          # SCServo moving status threshold
+SCS_MOVING_SPEED            = 500           # SCServo moving speed
+SCS_MOVING_ACC              = 0          # SCServo moving acc
+protocol_end                = 0           # SCServo bit end(STS/SMS=0, SCS=1)
+
+index = 0
+scs_goal_position = [SCS_MINIMUM_POSITION_VALUE, SCS_MAXIMUM_POSITION_VALUE]         # Goal position
+
+class servo_controller:
+    def __init__(self):    
+
+        # Initialize PortHandler instance
+        # Set the port path
+        # Get methods and members of PortHandlerLinux or PortHandlerWindows
+        self.portHandler = PortHandler(DEVICENAME)
+
+        # Initialize PacketHandler instance
+        # Get methods and members of Protocol
+        self.packetHandler = PacketHandler(protocol_end)
+
+        # Open port
+        if self.portHandler.openPort():
+            print("Succeeded to open the port")
+        else:
+            print("Failed to open the port")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+        # Set port baudrate
+        if self.portHandler.setBaudRate(BAUDRATE):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+
+    def setAcc(self):
+        # Write SCServo acc
+        scs_comm_result, scs_error = self.packetHandler.write1ByteTxRx(self.portHandler, SCS_ID, ADDR_SCS_GOAL_ACC, SCS_MOVING_ACC)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(scs_error))
+
+    def setSpeed(self,speed_val=0):
+
+        # Write SCServo speed
+        scs_comm_result, scs_error = self.packetHandler.write2ByteTxRx(self.portHandler, SCS_ID, ADDR_SCS_GOAL_SPEED, speed_val)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(scs_error))
+
+    def setPosition(self,position_val=0):
+
+            # Write SCServo goal position
+            scs_comm_result, scs_error = \
+                self.packetHandler.write2ByteTxRx\
+                (self.portHandler, SCS_ID, ADDR_SCS_GOAL_POSITION,servo_pose)
+            if scs_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(scs_comm_result))
+            elif scs_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(scs_error))
+
+    def getPoseSpeed(self):
+        
+        scs_present_position_speed, scs_comm_result, scs_error =\
+            self.packetHandler.read4ByteTxRx(self.portHandler, SCS_ID, ADDR_SCS_PRESENT_POSITION)
+        if scs_comm_result != COMM_SUCCESS:
+            print(self.packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print(self.packetHandler.getRxPacketError(scs_error))
+
+        scs_present_position = SCS_LOWORD(scs_present_position_speed)
+        scs_present_speed = SCS_HIWORD(scs_present_position_speed)
+        print("[ID:%03d] PresPos:%03d PresSpd:%03d" 
+              % (SCS_ID, scs_present_position, SCS_TOHOST(scs_present_speed, 15)))
+
+
+
+
+    def deactivateTorque(self):
+
+        scs_comm_result, scs_error = self.packetHandler.write1ByteTxRx(self.portHandler, SCS_ID, ADDR_SCS_TORQUE_ENABLE, 0)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(scs_error))
+
+    def closePort(self):
+        # Close port
+        self.portHandler.closePort()
+
+
+
+if __name__ == '__main__':
+    servoCtl = servo_controller()
+
+    while (True):
+        user_msg = input("Input servo position: (or press ESC to quit!)")
+        servo_speed = int(user_msg[0])*200            
+        servo_pose = int(user_msg[1])*1000            
+        if user_msg == chr(0x1b):
+            break            
+
+        print("User given speed: "+str(servo_speed))
+        print("User given position: "+str(servo_pose))
+
+        servoCtl.setPosition(servo_pose)
+        servoCtl.setSpeed(servo_speed)
+        servoCtl.getPoseSpeed()
