232cf6934fe0f8932738f592a5b49af9d8f10ce0
tzq <tangzhiqiang@yihang.ai>
2022-04-17 16:06:07 +0800

    almost done for mulit servo commu. two main todo things: 1. control servo when there are are squence of movements. 2. convert servo params such as speed, pose, and torque to deg/s , deg,newton

--- multi_servo_controller.py
+++ multi_servo_controller.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             send_data[i]['send_servo_speed_val'] = 500
             send_data[i]['send_servo_torque_val'] = 200
         multi_servo_ctl.push_to_send_queue(send_data)        
-        time.sleep(10)
+        time.sleep(5)
 
 
         send_data = copy.deepcopy(multi_servo_ctl.servo_commu_template)
@@ -137,6 +137,6 @@ if __name__ == '__main__':
             send_data[i]['send_servo_speed_val'] = 1000
             send_data[i]['send_servo_torque_val'] = 500
         multi_servo_ctl.push_to_send_queue(send_data)        
-        time.sleep(10)
+        time.sleep(5)
 
         print("in main")
